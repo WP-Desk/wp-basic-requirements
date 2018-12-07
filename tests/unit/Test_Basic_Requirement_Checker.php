cc03850ed68ce76cc9889c75ab6b2e44bd614668
dyszczo <krzysiek@inspirelabs.pl>
2018-12-07 14:38:01 +0100

    tests fix

--- tests/unit/Test_Basic_Requirement_Checker.php
+++ tests/unit/Test_Basic_Requirement_Checker.php
@@ -160,7 +160,8 @@ class Test_Basic_Requirement_Checker extends PHPUnit\Framework\TestCase {
 			[ $requirements, 'render_notices_action' ] );
 
 		$this->assertFalse( $requirements->are_requirements_met() );
-		$requirements->disable_plugin_render_notice();
+		$requirements->disable_plugin();
+        $requirements->render_notices();
 
 		WP_Mock::wpFunction( 'deactivate_plugins' )
 		       ->once();
